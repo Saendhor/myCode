0983a16337477a060a49fe10fa5e32510e3f16e7
Saendhor <comix143.mariorossi@gmail.com>
2025-10-09 19:22:11 +0200

    feat: implemented node.h, search, print and insert functions; todo: deletion function

--- bst/search_node.h
+++ bst/search_node.h
@@ -3,13 +3,24 @@
 
 #include <stdio.h>
 #include "node.h"
-int search_key (node_t* root, int key) {
+
+node_t* search_node (node_t* root, int key) {
+
     //key is in root
     if (root == NULL || root->key == key) {
-        return root->key;
+        return root;
     }
     //key is in left subtree
-    if (root->key)
+    if (root->key > key) {
+        return search_node(root->left, key);
+    }
 
+    //key is in right subtree
+    if (root->key < key) {
+        return search_node(root->right, key);
+    }
+
+    //throw error?
+    return NULL;
 }
 #endif
